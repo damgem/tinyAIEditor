9b516cc80a10cd754f6f9ebbec899f47867d3890
Damian Gleumes <damgem@zoho.com>
2023-10-24 22:28:39 +0200

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@bg-dev/nuxt-naiveui'
+  ],
+  build: {
+    transpile: [
+      'trpc-nuxt'
+    ]
+  },
+  typescript: {
+    shim: false
+  }
+})
